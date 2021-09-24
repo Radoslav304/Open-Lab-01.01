805c09897b4a4601acbac046d5ffa32a20e1381c
Radoslav304 <radoslavcabadaj304@gmail.com>
2021-09-24 08:46:11 +0200

    Parse input string to number and check if it's less than 10.

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -6,7 +6,16 @@ namespace Open_Lab_01._01
     {
         public bool IsLessThan10(string input)
         {
-            throw new NotImplementedException();
+            int uncharted;
+            Int32.TryParse(input, out uncharted);
+            if ( uncharted< 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
